32c2eed20628f5ceaba749f3bffdbf39c0ff7da5
vicenterj1 <vicenterj1@gmail.com>
2023-01-28 17:15:33 -0300

    Final tests

--- src/exercicioTestesJava/tests/FinancingTests.java
+++ src/exercicioTestesJava/tests/FinancingTests.java
@@ -65,5 +65,77 @@ public class FinancingTests {
 		
 	}
 	
+	@Test
+	public void setIncomeShouldSetDataWhenValidData() {
+		
+		//arrange
+		Financing f = new Financing (100000.00, 2000.00, 80);
+		
+		//action
+		f.setIncome(2100.0);
+		
+		//assert
+		Assertions.assertEquals(2100.00, f.getIncome());
+	}
+
+	@Test
+	public void setIncomeShouldThrowIllegalArgumentExceptionWhenInvalidData() {
+		
+		Assertions.assertThrows(IllegalArgumentException.class,
+				() -> {
+					//arrange
+					
+					//action
+					Financing f = new Financing (100000.00, 1900.0, 80);
+
+					//assert
+					f.setTotalAmount(1900.0);
+				});
+		
+	}
+
+	@Test
+	public void setMonthsShouldSetDataWhenValidData() {
+		
+		//arrange
+		Financing f = new Financing (100000.00, 2000.00, 80);
+		
+		//action
+		f.setMonths(81);
+		
+		//assert
+		Assertions.assertEquals(81, f.getMonths());
+	}
+
+	@Test
+	public void setMonthsShouldThrowIllegalArgumentExceptionWhenInvalidData() {
+		
+		Assertions.assertThrows(IllegalArgumentException.class,
+				() -> {
+					//arrange
+					
+					//action
+					Financing f = new Financing (100000.00, 2000.0, 80);
+
+					//assert
+					f.setMonths(79);
+				});
+		
+	}
+	
+
+	@Test
+	public void entryShouldCalculateEntryCorrectly() {
+		Financing f = new Financing (100000.00, 2000.0, 80);
+		
+		Assertions.assertEquals(20000.0, f.entry());
+	}
+	
+	@Test
+	public void quotaShouldCalculateQuotaCorrectly() {
+		Financing f = new Financing (100000.00, 2000.0, 80);
+		
+		Assertions.assertEquals(1000.0, f.quota());
+	}
 	
 }
